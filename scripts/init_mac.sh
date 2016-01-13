9a585f9d73a471914bd905151b671d37b3c67f5f
Jason Huang <f230536@hotmail.com>
2016-01-13 10:43:59 -0800

    update sh

--- scripts/init_mac.sh
+++ scripts/init_mac.sh
@@ -1,1 +1,9 @@
 curl -O http://selenium-release.storage.googleapis.com/2.48/SafariDriver.safariextz
+
+curl -O https://github.com/downloads/mackyle/xar/xar-1.6.1.tar.gz
+./configure
+make
+sudo make install
+sudo ln -s /full/path/to/xar-1.6.1/src/xar /usr/local/bin/xar161
+
+open SafariDriver.safariextz
