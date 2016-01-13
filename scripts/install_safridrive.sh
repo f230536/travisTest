10ac1e10d731cbb2dc7ae4a5b42b5340672d7c0c
Jason Huang <f230536@hotmail.com>
2016-01-13 14:34:16 -0800

    test 1

--- scripts/install_safridrive.sh
+++ scripts/install_safridrive.sh
@@ -8,7 +8,7 @@ curl -O http://selenium-release.storage.googleapis.com/2.48/SafariDriver.safarie
 #sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "REPLACE INTO access values ('kTCCServiceAccessibility', 'com.apple.Terminal', 0, 1, 1, NULL, NULL);"
 
 #OSX 10.10
-#sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "REPLACE INTO access values ('kTCCServiceAccessibility', 'com.apple.Terminal', 0, 1, 1, NULL);"
+sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "REPLACE INTO access values ('kTCCServiceAccessibility', 'com.apple.Terminal', 0, 1, 1, NULL);"
 
 
 #osascript -e 'tell application "Safari"
