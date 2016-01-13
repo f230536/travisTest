99328bcf4d236f325618a500a7a2477c1cd76273
Jason Huang <f230536@hotmail.com>
2016-01-13 13:27:29 -0800

    set up safaridrive

--- scripts/install_safridrive.sh
+++ scripts/install_safridrive.sh
@@ -1,24 +1,28 @@
 # this first part might not be needed
+
+curl -O http://selenium-release.storage.googleapis.com/2.48/SafariDriver.safariextz
+# this first part might not be needed
 curl -O http://selenium-release.storage.googleapis.com/2.48/SafariDriver.safariextz
 
 # OSX 10.11
 #sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "REPLACE INTO access values ('kTCCServiceAccessibility', 'com.apple.Terminal', 0, 1, 1, NULL, NULL);"
 
 #OSX 10.10
-sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "REPLACE INTO access values ('kTCCServiceAccessibility', 'com.apple.Terminal', 0, 1, 1, NULL);"
+#sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "REPLACE INTO access values ('kTCCServiceAccessibility', 'com.apple.Terminal', 0, 1, 1, NULL);"
 
+
+#osascript -e 'tell application "Safari"
+#  activate
+#end tell'
 open SafariDriver.safariextz
+
 osascript -e '
 tell application "System Events"
   tell process "Safari"
-    delay 3
-    click button "Trust" of sheet 1 of window 1
-    delay 
-    quit
+    set frontmost to true
+      repeat until (exists window 1) and subrole of window 1 is "AXDialog" -- wait until the dialog is displayed.
+        delay 1
+      end repeat
+    click button 1 of front window -- Trust
   end tell
-end tell
-
-tell application "Safari"
-  quit
-end tell
-'
+end tell'
